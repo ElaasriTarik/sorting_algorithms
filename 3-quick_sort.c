8f1646d8e3c56d0faa2e1183ea7684e6e5f8e3ac
FahdAzouz <fahdstudies00@gmail.com>
2023-10-19 14:30:53 +0100

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,10 +8,10 @@
  */
 void swap(int *a, int *b)
 {
-    int tmp = *a;
+	int tmp = *a;
 
-    *a = *b;
-    *b = tmp;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -24,27 +24,27 @@ void swap(int *a, int *b)
  */
 int partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1, j;
+	int pivot = array[high];
+	int i = low - 1, j;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap(&array[i], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
-    if (i + 1 != high)
-    {
-        swap(&array[i + 1], &array[high]);
-        print_array(array, size);
-    }
-    return (i + 1);
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
 }
 
 /**
@@ -58,14 +58,14 @@ int partition(int *array, int low, int high, size_t size)
 
 void quicksort(int *array, int low, int high, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (low < high)
-    {
-        pivot = partition(array, low, high, size);
-        quicksort(array, low, pivot - 1, size);
-        quicksort(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -76,7 +76,7 @@ void quicksort(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
-    quicksort(array, 0, size - 1, size);
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
 }
